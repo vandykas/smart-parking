4b779e9038f64e6b9588ab99d98f1e11b80089a9
bryan's_laptop <bryan>
2026-01-11 09:50:24 +0700

    fixing

--- src/main/java/com/example/smart_parking/controller/HomeController.java
+++ src/main/java/com/example/smart_parking/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @GetMapping("/")
     public String viewRoot() {
-        return "redirect:/home/";
+        return "welcomePage";
     }
 
     @GetMapping("/home/")
